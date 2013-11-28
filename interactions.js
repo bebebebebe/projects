818ceed60e1b65c2a27e2eacd905e090e1c8ca42
Be <bebirchall@gmail.com>
2013-11-27 23:07:16 -0500

    start js interactions

--- interactions.js
+++ interactions.js
@@ -3,11 +3,32 @@ $(document).ready(function() {
   $('.project').on({
     mouseenter: function () {
         $(this).children('.project-description').css('color', 'black');
+        $(this).children('.project-title').css('font-weight', 'bold');
     },
     mouseleave: function () {
+        $('.project-title').css('font-weight', '');
         $('.project-description').css('color', '#939393');
     }
-});
+  });
+
+  $('#visualschemer').on({
+    mouseenter: function () {
+      $('#picture').css('background', 'orange');
+    },
+    mouseleave: function () {
+      $('#picture').css('background', '');
+    }
+  });
+
+
+   $('#schemerb').on({
+    mouseenter: function () {
+      $('#picture').css('background', 'purple');
+    },
+    mouseleave: function () {
+      $('#picture').css('background', '');
+    }
+  });
 
 
 });
