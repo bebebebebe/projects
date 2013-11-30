3b46b77a68d396fec3c6e3524a599df33f57d502
Be <bebirchall@gmail.com>
2013-11-29 22:33:00 -0500

    add a picture, add basic project info

--- interactions.js
+++ interactions.js
@@ -13,17 +13,19 @@ $(document).ready(function() {
 
   $('#visualschemer').on({
     mouseenter: function () {
-      $('#picture').css('background', 'orange');
+      $('#picture').html("<img src='pictures/schemetree.jpg'/>");
+      //$('#picture').css('background', 'orange');
     },
     mouseleave: function () {
-      $('#picture').css('background', '');
+      //$('#picture').css('background', '');
+      $('#picture').html("<p>An overview of my programming projects with links to demos/deployed apps.</p><p><a href='https://github.com/bebebebebe'>GitHub profile</a></p>");
     }
   });
 
 
    $('#schemerb').on({
     mouseenter: function () {
-      $('#picture').css('background', 'purple');
+     // $('#picture').css('background', 'purple');
     },
     mouseleave: function () {
       $('#picture').css('background', '');
