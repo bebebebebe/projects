1b4b5588b21f87ddd7aada58ce63f36f8baffad5
Be <bebirchall@gmail.com>
2014-03-09 19:04:34 -0400

    add wmf parsoid project

--- project_data.js
+++ project_data.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
   $(function  () {
     var projectsData = [
       {
+        title: "Wikimedia Parsoid: Open Source Contributions",
+        description: 'I\'m currently doing a Gnome Foundation OPW internship for open source projects. I\'m working with Wikimedia\'s Parsoid team. Parsoid translates back and forth between wikitext and HTML. Its main use at the moment is in Wikimedia\'s Visual Editor, which gives people who aren\'t familiar with HTML or wikitext a user friendly interface for editing Wikipedia pages. My project involves Parsoid\'s extensive testing framework: I\'m working on both front end and back end improvements so the development team can better interact with the testing results. I\'m using Node.js, MySQL, JavaScript, JQuery, HTML, CSS and Handlebars templating.',
+        code: 'https://github.com/wikimedia/mediawiki-services-parsoid/commits?author=bebebebebe'},
+      {
         title: "Visual Schemer", 
         description: "In-browser version of SchemerB (below), with semantic visualizations. As you enter Scheme expressions in the REPL via the in-browser console, tree pictures are drawn showing the environment structure. Hovering over a node in the tree reveals the bindings present at that node. This is a Sinatra app; visualizations are made with D3.js.",
         demo: 'http://visual-schemer.herokuapp.com/',
@@ -33,12 +37,12 @@ $(document).ready(function(){
         description: "Ruby on Rails app for versioned modular writing, modeling stacks of index cards. When composing text on a 'card,' you can click a 'keep version' button and continue revising. All versions are kept in a 'stack,' with the text from each top card contributing to your document. You can change your mind about which card to select on top of a stack in a view displaying word based diffs.",
         code: 'https://github.com/bebebebebe/palefire',
       },
-      {
-        title: 'StreetEdit',
-        description: "App for finding, editing and making galleries of images from Google Street View. Uses Google Street View and Aviary Photo editing API's",
-        code: 'https://github.com/bebebebebe/street-editor',
-        picture: 'pictures/street.png'
-      },
+      // {
+      //   title: 'StreetEdit',
+      //   description: "App for finding, editing and making galleries of images from Google Street View. Uses Google Street View and Aviary Photo editing API's",
+      //   code: 'https://github.com/bebebebebe/street-editor',
+      //   picture: 'pictures/street.png'
+      // },
       {
         title: 'Cheating Hangman',
         description: "Ruby command line program that cheats while playing hangman. You guess letters, and the computer keeps 'changing its mind' about what the secret word is in response to your guesses.",
@@ -55,6 +59,7 @@ $(document).ready(function(){
     var theTemplateScript = $("#projects-template").html();
 
     var theTemplate = Handlebars.compile(theTemplateScript);
+    
     $('#projects-list').append(theTemplate(projectsData));
 
   });
